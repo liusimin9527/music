2eca4fa292e0a515610d409a2c2a2d154b54b674
liusimin9527 <1246920026@qq.com>
2017-09-05 23:48:39 +0800

    v1.0.2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,18 +1,57 @@
-EventUtil.addHandler(window, 'load', function() {
-	var searchText = document.getElementById('search-text');
-	var searchBtn = document.getElementById('search-btn');
-	var prev = document.getElementById('prev');
-	var play = document.getElementById('play');
-	var next = document.getElementById('next');
-	var songName = document.getElementById('song-name');
+$(document).ready(function () {
+	/*  */
+	$('.top_nav__item a').click(function () {
+		$(this).siblings('a').removeClass('top_nav__link--current');   //删除样式
+		$(this).addClass('top_nav__link--current');  //添加样式
+	});
+	/* 显示搜索结果 */
+	$('.search_input__input').focus(function () {
+		//$('.js_smartbox').show();
+	});
+	/* 搜索 */
+	$('.search_input__btn').click(function () {
+		$('.search_input__input').val();  //数据
+		$('.search_history_item');  //增加历史记录
+	});
+	/* 登录 */
+	$('.js_login').click(function () {
 
-	music.init(); //初始化
-	music.addMusic();
+		//显示用户信息
+		$('.popup_user').show();
+	});
+	/* 回到顶部 */
+	var clientHeight = document.documentElement.clientHeight;
+	var timer = null;
+	var	isTop = true;
 
-	EventUtil.addHandler(searchBtn, 'click', function() {
-		AjaxUtil.get('/music' + searchText.value, function() {
+	//滚动条滚动
+	$(window).scroll(function () {
+		var top = document.documentElement.scrollTop || document.body.scrollTop;
 
-			//location.href = '/music';
-		});
+		if (top >= clientHeight) {
+			$('.js_btn_top').show();
+		} else {
+			$('.js_btn_top').hide();
+		}
+
+		if (!isTop) {
+			clearInterval(timer);
+		}
+		isTop = false;
+	});
+	$('.js_btn_top').click(function () {
+		clearInterval(timer);
+
+		timer = setInterval(function () {
+			var top = document.documentElement.scrollTop || document.body.scrollTop;
+			var ispeed = Math.ceil(top/6);//向上舍入
+
+			document.documentElement.scrollTop = document.body.scrollTop = top - ispeed;
+
+			isTop = true;
+			if(top == 0){
+				clearInterval(timer);
+			}
+		}, 30);
 	});
 });
--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
-/*  */
+/* 首页 */
 router.get('/', function(req, res, next) {
   res.render('index', {
-  	"title": "QQ音乐"
+  	title : "QQ音乐"
   });
 });
-
 /* 歌手 */
 router.get('/portal/singer_list.html', function(req, res, next) {
 	res.render('singer_list', {
-		"title": "歌手列表-QQ音乐"
+		title : "歌手列表-QQ音乐"
 	});
 });
 /* 专辑 */
 router.get('/portal/album_lib.html', function(req, res, next) {
 	res.render('album_lib', {
-		"title": "专辑库-QQ音乐-中国最新最全的免费正版高品质无损音乐平台"
+		title : "专辑库-QQ音乐-中国最新最全的免费正版高品质无损音乐平台"
+	});
+});
+/* 排行榜 */
+router.get('', function(req, res, next) {
+	res.render('', {
+
 	});
 });
 /* 分类歌单 */
 router.get('/portal/playlist.html', function(req, res, next) {
 	res.render('playlist', {
-		"title": "分类歌单-QQ音乐-中国最新最全的免费正版高品质无损音乐平台"
+		title : "分类歌单-QQ音乐-中国最新最全的免费正版高品质无损音乐平台"
 	});
 });
 /* 电台 */
 router.get('/portal/radio.html', function(req, res, next) {
 	res.render('radio', {
-		"title": "电台-QQ音乐-中国最新最全的免费正版高品质无损音乐平台"
+		title : "电台-QQ音乐-中国最新最全的免费正版高品质无损音乐平台"
 	})
 })
 /* 数字专辑 */
 router.get('/portal/album_mall.html', function(req, res, next) {
 	res.render('album_mall', {
-		"title": "数字专辑-QQ音乐-中国最新最全的免费正版高品质无损音乐平台"
+		title : "数字专辑-QQ音乐-中国最新最全的免费正版高品质无损音乐平台"
 	});
 });
 /* 我的音乐 */
 router.get('/portal/profile.html', function(req, res, next) {
 	res.render('profile.jade', {
-		"title": "我的音乐-QQ音乐-中国最新最全的免费正版高品质无损音乐平台"
+		title : "我的音乐-QQ音乐-中国最新最全的免费正版高品质无损音乐平台"
 	});
 })
 /* 客户端 */
